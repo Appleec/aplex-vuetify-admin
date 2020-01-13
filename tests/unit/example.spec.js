cab65e3728fb1421a31ebf114b3cf0e636ad1712
appleex <linzy@seirobotics.net>
2020-01-13 19:33:47 +0800

    add(tests): add jest unit test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,13 +1,37 @@
-import { expect } from 'chai'
-import { shallowMount } from '@vue/test-utils'
-// import HelloWorld from '@/components/HelloWorld.vue'
+import { mount, createLocalVue } from '@vue/test-utils'
+
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+
+Vue.use(Vuetify)
+
+import CustomCard from '@/components/Example.vue'
+
+const localVue = createLocalVue()
 
 describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    // const msg = 'new message'
-    // const wrapper = shallowMount(HelloWorld, {
-    //   propsData: { msg }
-    // })
-    // expect(wrapper.text()).to.include(msg)
+  let vuetify
+
+  beforeEach(() => {
+    vuetify = new Vuetify()
+  })
+
+  it('should have a custom title and match snapshot', () => {
+    const wrapper = mount(CustomCard, {
+      localVue,
+      vuetify,
+      propsData: {
+        title: 'Foobar'
+      }
+    })
+
+    // With jest we can create snapshot files of the HTML output
+    // expect(wrapper.html()).toMatchSnapshot()
+
+    // We could also verify this differently
+    // by checking the text content
+    const title = wrapper.find('.v-card__title > span')
+
+    expect(title.text()).toBe('Foobar')
   })
 })
